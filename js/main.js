29945ffb901d2b35f425d5848d955a549566245a
Ibrahim Mohammed <56862776+IbrahimMohammed017@users.noreply.github.com>
2025-03-13 11:08:52 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 // Navbar Background
-// document.addEventListener("DOMContentLoaded", function () {
-//   const navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarToggler = document.querySelector(".navbar-toggler");
 
-//   window.addEventListener("scroll", function () {
-//     if (window.scrollY >= 125) {
-//       navbar.style.background = "rgba(22, 60, 144, 0.9)";
-//       navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.5)";
-//     } else {
-//       navbar.style.background = "transparent";
-//       navbar.style.boxShadow = "none";
-//     }
-//   });
-// });
+  function checkScroll() {
+    if (window.scrollY >= 150) {
+      navbar.style.background = "rgb(42 90 218 / 95%)";
+      navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.5)";
+    } else {
+      navbar.style.background = "transparent";
+      navbar.style.boxShadow = "none";
+    }
+  }
+
+  // تنفيذ الفحص عند تحميل الصفحة وأثناء التمرير
+  checkScroll();
+  window.addEventListener("scroll", checkScroll);
+
+  // عند الضغط على زر القائمة، يتم تلوين الـ navbar فورًا
+  navbarToggler.addEventListener("click", function () {
+    navbar.style.background = "rgb(42 90 218 / 95%)";
+    navbar.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.5)";
+  });
+});
 
 // Texts Of Slider
 document.addEventListener("DOMContentLoaded", function () {
@@ -28,8 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let isDeleting = false;
   let cursorVisible = true;
 
-  const speed = 40; // سرعة الكتابة والحذف
-  const holdTime = 2000; // المدة الزمنية للاحتفاظ بالنص الكامل قبل البدء في الحذف
+  const speed = 40;
+  const holdTime = 2000;
 
   const pElement = document.querySelector(".slider-text");
   const cursorElement = document.createElement("span");
@@ -68,159 +79,60 @@ document.addEventListener("DOMContentLoaded", function () {
   blinkCursor();
 });
 
-// Class Active For Nav-Link
-// document.addEventListener("DOMContentLoaded", function () {
-//   let list = document.querySelectorAll(".nav-item a");
-
-//   list.forEach((li) => {
-//     li.addEventListener("click", removeActive);
-//   });
-
-//   function removeActive() {
-//     list.forEach((li) => {
-//       li.classList.remove("active");
-//       this.classList.add("active");
-//     });
-//   }
-// });
-
 // Navbar Collapse
-// document.addEventListener("DOMContentLoaded", function () {
-//   function toggleNavbarLinks() {
-//     if (window.innerWidth < 992) {
-//       const navLinks = document.querySelectorAll(".nav-link");
-//       navLinks.forEach((link) => {
-//         link.addEventListener("click", function (event) {
-//           event.preventDefault();
-//           if (this.parentNode.classList.contains("dropdown")) {
-//             return;
-//           }
-//           const navbarCollapse = document.getElementById(
-//             "navbarSupportedContent"
-//           );
-//           const bsCollapse = new bootstrap.Collapse(navbarCollapse);
-//           bsCollapse.toggle();
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbarCollapse = document.querySelector("#navbarSupportedContent");
 
-//           var target = this.getAttribute("href");
-//           if (target && target != "#") {
-//             setTimeout(function () {
-//               window.location.href = target;
-//             }, 350);
-//           }
-//         });
-//       });
-//     } else {
-//       const navLinks = document.querySelectorAll(".nav-link");
-//       navLinks.forEach((link) => {
-//         link.removeEventListener("click", function () {});
-//       });
-//     }
-//   }
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      if (window.innerWidth < 992) {
+        navbarCollapse.classList.remove("show"); // إزالة "show" لإغلاق القائمة
+        navbarToggler.setAttribute("aria-expanded", "false"); // تحديث حالة الزر
+      }
+    });
+  });
+});
 
-//   toggleNavbarLinks();
-//   window.addEventListener("resize", toggleNavbarLinks);
-// });
+// Class Active For Nav-Link
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("article, header");
+  const navLinks = document.querySelectorAll(".nav-item a");
 
-// Counter
-// document.addEventListener("DOMContentLoaded", function () {
-//   const counters = document.querySelectorAll(".purecounter");
+  function setActiveNav() {
+    let scrollPosition = window.scrollY;
+    let foundSection = false;
 
-//   // إنشاء observer لمراقبة ظهور العناصر
-//   const observer = new IntersectionObserver(
-//     (entries, observer) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add("visible"); // إضافة كلاس لتفعيل ظهور العنصر
-//           startCounting(entry.target); // بدء العد عند ظهور العنصر
-//           observer.unobserve(entry.target); // التوقف عن مراقبة العنصر بعد التفاعل معه
-//         }
-//       });
-//     },
-//     { threshold: 1.0 }
-//   ); // يجب أن يظهر العنصر بالكامل في الشاشة (100%)
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute("id");
 
-//   counters.forEach((counter) => {
-//     observer.observe(counter); // مراقبة العناصر
-//   });
+      if (
+        scrollPosition >= sectionTop &&
+        scrollPosition < sectionTop + sectionHeight
+      ) {
+        navLinks.forEach((link) => {
+          if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active");
+          } else {
+            link.classList.remove("active");
+          }
+        });
+        foundSection = true;
+      }
+    });
 
-//   // وظيفة العد
-//   function startCounting(element) {
-//     const start = 0; // العد يبدأ من 0
-//     const end = parseInt(element.getAttribute("data-purecounter-end"), 10);
-//     const duration = 2; // الزمن 2 ثانية
-
-//     // التأكد من أن جميع القيم أرقام صالحة
-//     if (isNaN(end)) {
-//       console.error("هناك مشكلة في القيم المدخلة!");
-//       return;
-//     }
-
-//     let current = start;
-//     const increment = (end - start) / ((duration * 1000) / 50); // حساب الزيادة في كل خطوة خلال 2 ثانية
-
-//     const interval = setInterval(() => {
-//       current += increment;
-//       if (current >= end) {
-//         clearInterval(interval);
-//         element.innerText = end; // عند الوصول للقيمة النهائية
-//       } else {
-//         element.innerText = Math.round(current); // عرض القيمة الحالية مع تقريبها
-//       }
-//     }, 50); // كل 50 ميلي ثانية
-//   }
-// });
-
-// MixItUp For Portfolio
-// document.addEventListener("DOMContentLoaded", function () {
-//   var containerEl = document.querySelector(".mix-filter");
-//   var mixer = mixitup(containerEl);
-// });
-
-// Class Active For Portfolio
-// document.addEventListener("DOMContentLoaded", function () {
-//   let list = document.querySelectorAll(".gallery button");
-
-//   list.forEach((li) => {
-//     li.addEventListener("click", removeShow);
-//   });
-
-//   function removeShow() {
-//     list.forEach((li) => {
-//       li.classList.remove("show");
-//       this.classList.add("show");
-//     });
-//   }
-// });
-
-// Testimonials
-// document.addEventListener("DOMContentLoaded", function () {
-//   new Swiper(".testimonials-slider", {
-//     speed: 600,
-//     loop: true,
-//     autoplay: {
-//       delay: 5000,
-//       disableOnInteraction: false,
-//     },
-//     slidesPerView: "auto",
-//     pagination: {
-//       el: ".swiper-pagination",
-//       type: "bullets",
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       320: {
-//         slidesPerView: 1,
-//         spaceBetween: 40,
-//       },
-
-//       1200: {
-//         slidesPerView: 3,
-//       },
-//     },
-//   });
-// });
-
-// #######################
+    if (!foundSection && scrollPosition === 0) {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      document
+        .querySelector(".nav-item a[href='#home']")
+        .classList.add("active");
+    }
+  }
+  window.addEventListener("scroll", setActiveNav);
+});
 
 // Toggle Themes
 document.addEventListener("DOMContentLoaded", function () {
@@ -233,10 +145,18 @@ document.addEventListener("DOMContentLoaded", function () {
       root.style.setProperty("--text-color", "#333");
       root.style.setProperty("--bgColor", "#fff");
       root.style.setProperty("--story-img", "#f5f5f5");
+      root.style.setProperty(
+        "--gradient",
+        "linear-gradient(0deg, rgba(198, 198, 198, 0.7), rgba(255, 255, 255, 0.3), rgba(198, 198, 198, 0.7))"
+      );
     } else {
       root.style.setProperty("--text-color", "#ccc");
       root.style.setProperty("--bgColor", "#000");
       root.style.setProperty("--story-img", "#222");
+      root.style.setProperty(
+        "--gradient",
+        "linear-gradient(0deg, rgba(20, 20, 20, 0.7), rgba(50, 50, 50, 0.3), rgba(20, 20, 20, 0.7))"
+      );
     }
     localStorage.setItem("theme", theme);
   }
@@ -250,3 +170,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Update Of The Year On Footer
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// #######################
